af0892ff6c6c363a7635d134469512d22b12d9cb
ZanAmbrozic <zan.ambrozic@gmail.com>
2023-12-04 12:53:34 +0100

    Refactor

--- Day_04/main.py
+++ Day_04/main.py
@@ -15,20 +15,18 @@ print(total)
 
 #  Part 2
 
-file = open("input.txt")
-all_lines = file.read().splitlines()
-file.close()
-
-
 total = 0
 totals = {}
-
-for i, line in enumerate(all_lines, start=1):
+i = 1
+for line in open("input.txt"):
     winning, yours = line.split(":")[1].split("|")
     exp = len(set(winning.split()) & set(yours.split()))
     totals[i] = totals.get(i, 0) + 1
+
     for e in range(1, exp + 1):
         totals[i + e] = totals.get(i + e, 0) + 1 * totals[i]
+
+    i += 1
 
 total = sum(totals.values())
 print(total)
